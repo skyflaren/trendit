fbb777e57926887365eb12e068de0eed80aad5cc
RonaldYou <ronaldrenyou@gmail.com>
2020-06-20 16:35:17 -0400

    userInput

--- backend.py
+++ backend.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run()
