46db0bcb7d789ce2d1cff06952d0801931ff17ef
KennethRuan <kennethjruan@gmail.com>
2020-06-20 20:19:40 -0400

    added function to retrieve top search queries

--- retrieve_trends.py
+++ retrieve_trends.py
@@ -1,47 +1,97 @@
 from pytrends.request import TrendReq
-from datetime import datetime
+from datetime import datetime, timedelta
+from collections import defaultdict
 import pandas as pd
 
-pytrends = TrendReq(hl='en-US', tz=360)
-# pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), proxies=['https://34.203.233.13:80',], retries=2, backoff_factor=0.1, requests_args={'verify':False})
+
+def get_query(ind, topic, timeframe):
+    query_freq = defaultdict(int)
+
+    pytrends = TrendReq(hl='en-US', tz=360)
+    # pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), proxies=['https://34.203.233.13:80',], retries=2, backoff_factor=0.1, requests_args={'verify':False})
+
+    current_time = datetime.now()
+
+    old_time = current_time
+
+    if timeframe == "1-y":
+        old_time = current_time - timedelta(days=365)
+    elif timeframe == "1-m":
+        old_time = current_time - timedelta(days=30)
+    elif timeframe == "7-d":
+        old_time = current_time - timedelta(days=7)
+
+    print("Time frame:")
+    print(str(old_time) + " " + str(current_time))
+
+    kw_list = [topic]
+
+    # converts datetime to date objects
+    old_date = datetime.date(old_time)
+    current_date = datetime.date(current_time)
+
+    pytrends.build_payload(kw_list, cat=0, timeframe= str(old_date) + " " + str(current_date), geo='', gprop='')
+
+    interest_over_time = pytrends.interest_over_time()
+
+    if interest_over_time.empty:
+        return None
+
+    largest_interest = interest_over_time.sort_values(by=[topic], ascending=False)
+
+    largest_interest_dates = largest_interest.index
+
+    # for index, row in pytrends.related_topics()[topic]['top'].iterrows():
+    #     print(row)
+
+    print(largest_interest)
+
+    print("Day: " + str(largest_interest_dates[ind]))
+
+    search_time = pd.Timestamp.to_pydatetime(largest_interest_dates[ind])
+    search_date_first = datetime.date(search_time)
+    search_date_second = datetime.date(search_time)
+
+    # related_queries = pd.DataFrame(zip(pd.DataFrame(zip([],[]), columns=['top', 'rising'])),columns=[topic])
+    related_queries = None
+    cnt = 0
+
+    while related_queries is None:
+        cnt += 1
+        search_date_first = search_date_first - timedelta(days=1)
+        pytrends.build_payload(kw_list, cat=0, timeframe= str(search_date_first) + " " + str(search_date_second), geo='', gprop='')
+        related_queries = pytrends.related_queries()[topic]['top']
+
+        if cnt > 10:
+            break
+
+    if related_queries is not None:
+        print(search_date_first)
+        print(search_date_second)
+
+        print(related_queries)
+
+        for index, row in related_queries.iterrows():
+            if index > 9:
+                break
+            query_freq[row['query']] += row['value']
+
+    return [search_date_first.strftime("%Y/%m/%d"), search_date_second.strftime("%Y/%m/%d"), sorted(query_freq.items(), key = lambda v: v[1], reverse=True)]
+
 
 topic = input("What would you like to search for? ")
-current_time = datetime.now()
 
 timeframe = ""
-options = ["1-y","1-m","1-d"]
+options = ["1-y","1-m","7-d"]
 
 # gets time frame input
 while timeframe not in options:
     timeframe = input("Select a timeframe (1-y, 1-m, 7-d): ")
 
-old_time = current_time
 
-if timeframe == "1-y":
-    old_time = datetime(current_time.year - 1, current_time.month, current_time.day)
-
-print(str(old_time) + " " + str(current_time))
-
-kw_list = [topic]
-
-# converts datetime to date objects
-old_date = datetime.date(old_time)
-current_date = datetime.date(current_time)
-
-pytrends.build_payload(kw_list, cat=0, timeframe= str(old_date) + " " + str(current_date), geo='', gprop='')
-
-interest_over_time = pytrends.interest_over_time()
-
-largest_interest = interest_over_time.sort_values(by=[topic], ascending=False)
-
-largest_interest_dates = largest_interest.index
-
-print(type(largest_interest_dates[0]))
-print(largest_interest_dates[0])
-
-search_time = pd.Timestamp.to_pydatetime(largest_interest_dates[0])
-search_date = datetime.date(search_time)
-
-pytrends.build_payload(kw_list, cat=0, timeframe= str(search_date) + " " + str(search_date), geo='', gprop='')
-
-print(pytrends.related_queries()[topic]['rising']['query'])
+for i in range(3):
+    query = get_query(i, topic, timeframe)
+    if query is None:
+        print("No large events found, please try refining your search terms")
+    else:
+        print(query)
