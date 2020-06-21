3ddcb9a12b2a33926235ebf9db5a112f843292d5
RonaldYou <ronaldrenyou@gmail.com>
2020-06-20 23:17:39 -0400

    gen 1 animation

--- static/js/moon.js
+++ static/js/moon.js
@@ -1,14 +1,37 @@
 let searchBar = document.getElementById("example");
 let moon = document.getElementById("moon");
 
-searchBar.addEventListener("mouseenter"){
+let enter, leave;
+
+searchBar.addEventListener("mouseenter", function(){
+    console.log("itworks");
+    var pos = moon.style.marginRight.replace('px', '');
+    clearInterval(leave);
+    clearInterval(enter);
     enter = setInterval(function(){
-
+        if(pos>233){
+            clearInterval(enter);
+        }
+        else{
+            pos++;
+            moon.style.marginRight = pos + 'px';
+        }
     }, 20);
-}
+})
 
-searchBar.addEventListener("mouseleave"){
+
+searchBar.addEventListener("mouseleave", function(){
+    console.log("itLEAVES");
+    var pos = moon.style.marginRight.replace('px', '');
+    clearInterval(enter);
+    clearInterval(leave);
     leave = setInterval(function(){
-
+        if(pos<50){
+            clearInterval(leave);
+        }
+        else{
+            pos--;
+            moon.style.marginRight = pos + 'px';
+        }
     }, 20);
-}
+})
