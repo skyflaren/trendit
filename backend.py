c196a28996107374cda4dd3ba17135bbca8e2b37
Justin Lu <wabasabi359@gmail.com>
2020-06-21 17:51:42 -0400

    minor backend changes

--- backend.py
+++ backend.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-
     if request.method == "POST":
         user = request.form["nm"]
         user = stripSpecial(user)
