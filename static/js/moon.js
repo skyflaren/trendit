9e955257d51fcdf612ad1f36ea92d4a4257c7c89
Justin Lu <wabasabi359@gmail.com>
2020-06-20 22:09:43 -0400

    set up basic js file

---
+++ static/js/moon.js
@@ -0,0 +1,14 @@
+let searchBar = document.getElementById("example");
+let moon = document.getElementById("moon");
+
+searchBar.addEventListener("mouseenter"){
+    enter = setInterval(function(){
+
+    }, 20);
+}
+
+searchBar.addEventListener("mouseleave"){
+    leave = setInterval(function(){
+
+    }, 20);
+}
