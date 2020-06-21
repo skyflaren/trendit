b5fe6efdd6c9cb26345d07dc46ad21e37b9b3335
RonaldYou <ronaldrenyou@gmail.com>
2020-06-21 13:18:28 -0400

    userInput

--- backend.py
+++ backend.py
@@ -9,17 +9,22 @@ def index():
 
     if request.method == "POST":
         user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+        quantity = request.form["quant"]
+        un = request.form["units"]
+        country = request.form["region"]
+        #flash(quantity);
+        return redirect(url_for("user", usr=user, quant = quantity, units = un, region = country))
+        #return redirect(url_for("user", usr=user))
     else:
         return render_template("index.html", trendingSearches=get_trending_list())
 
 
-@app.route("/<usr>")
-def user(usr):
+@app.route("/<usr>/<quant>/<units>/<region>")
+def user(usr,quant,units,region):
     if(usr == "results"):
-        return render_template("results.html", trendingSearches=get_trending_list());
+        return render_template("results.html", trendingSearches=get_trending_list())
     else:
-        return f"<h1>{usr}</h1>"
+        return f"<h1>{usr + quant + units + region}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
